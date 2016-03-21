5fcb4dfb66f9d6f30f17dc852d4ff323e245aa54
dyHub <dy_joy2012@126.com>
2016-03-21 16:06:54 -0700

    Initial Commit

---
+++ CampsIt/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CampsIt
+//
+//  Created by Yuhan Dai on 3/21/16.
+//  Copyright © 2016 Yuhan Dai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CampsIt/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CampsIt
+//
+//  Created by Yuhan Dai on 3/21/16.
+//  Copyright © 2016 Yuhan Dai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
